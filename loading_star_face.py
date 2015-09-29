2416c36f81c3c2c8679d70831c224e667d5f737e
dongshaohui <dongsh@jsfund.cn>
2015-09-29 22:05:42 +0800

    更新

--- loading_star_face.py
+++ loading_star_face.py
@@ -84,11 +84,15 @@ def loading_faceset(db,star_face_result,faceset_id):
 def pre_run(db):
 	male_star_face_result = db.select('select face_img_link,id from faceapp_star_user where is_valid = 1 and sex = "M"')
 	female_star_face_result = db.select('select face_img_link,id from faceapp_star_user where is_valid = 1 and sex = "F"')
-	loading_faceset(db,male_star_face_result,'dceed1f5a2b90814c26d10bd254a6f4c')
+	loading_faceset(db,female_star_face_result,'882895af96bce804ae701972f05e599b')
 
+
+# 检查图像文件是否存在人脸（即是否有效）
 def check_image_valid(db):
+	print 'check_image_valid'
 	api = API(API_KEY, API_SECRET)
-	all_result = db.select('select id,face_img_link from faceapp_star_user where is_valid = 1')
+	all_result = db.select('select id,face_img_link from faceapp_star_user where is_valid = 1 and id = 4081')
+	print all_result
 	for result in all_result:
 		p_id = result[0]
 		p_image = result[1]
@@ -99,6 +103,7 @@ def check_image_valid(db):
 			print p_id,'error'
 			continue
 		print p_id
+		print p_result
 		if len(p_result) != 1:
 			# change tag invalid
 			record = {}
@@ -107,8 +112,21 @@ def check_image_valid(db):
 			cond['id'] = p_id
 			update_record_db(db,record,cond,'faceapp_star_user')
 			
+# 看相似脸
+def find_similar_face():
+	print 'find_similar_face'
+	p_image = 'http://dongshaohui-oss.oss-cn-hangzhou.aliyuncs.com/1442737462.83Screenshot_2015-09-20-13-53-03.png'
+	params = urllib.urlencode({'api_key': API_KEY,'api_secret':API_SECRET})
+	api = API(API_KEY, API_SECRET)
+	p_result_face_id = api.detection.detect(url = p_image)['face'][0]['face_id']
+	find_params = urllib.urlencode({'api_key': API_KEY,'api_secret':API_SECRET,'key_face_id':p_result_face_id,'faceset_id':'882895af96bce804ae701972f05e599b'})
+	find_face_url = 'https://apicn.faceplusplus.com/v2/recognition/search?' + find_params
+	print find_face_url
+	f = urllib.urlopen(find_face_url)	
+	print f.read()
 
 if __name__ == '__main__':
 	db = Connent_Online_Mysql_By_DB('rdsjjuvbqjjuvbqout.mysql.rds.aliyuncs.com',3306,'dongsh','5561225','faceapp','/tmp/mysql.sock')
-	pre_run(db)
+	check_image_valid(db)
+	# find_similar_face()
 	
